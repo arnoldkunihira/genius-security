5c12a553afc2586806bb5b99875be2caf7c94b24
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-07-02 14:36:19 +0300

    Initial project commit with form based auth

---
+++ src/main/java/com/metaverse/geniussecurity/GeniusSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.geniussecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniusSecurityApplication.class, args);
+	}
+
+}
